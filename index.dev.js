46883ea57b2a72009f6ad6ec25e3f3352d275676
medo_wcc <belief.flychen@foxmail.com>
2023-08-22 14:17:14 +0800

    medo 定制

--- index.dev.js
+++ index.dev.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         Iconfont React
+// @name         Iconfont Medo
 // @version      1.0.0
 // @description  自动下载 React ICON 组件
 // @author       yee.wang
 // @include      https://*.iconfont.cn/*
 // @require      http://cdn.bootcss.com/jquery/1.12.4/jquery.min.js
-// @updateURL    https://github.com/yee94/tampermonkey-iconfont-iconify/raw/main/index.user.js
-// @downloadURL  https://github.com/yee94/tampermonkey-iconfont-iconify/raw/main/index.user.js
+// @updateURL    https://github.com/Daybreak-1024/tampermonkey-iconfont-iconify/raw/main/index.user.js
+// @downloadURL  https://github.com/Daybreak-1024/tampermonkey-iconfont-iconify/raw/main/index.user.js
 // @require  	 file:///Users/yee.wang/Code/libs/tampermonkey/iconfont-react/dist/main.js
 // @grant        unsafeWindow
 // @connect      *
--- index.user.js
+++ index.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         Iconfont React
-// @version      1.0.3
+// @name         Iconfont Medo
+// @version      1.0.0
 // @description  下载 React ICON 组件
 // @author       yee.wang
 // @include      https://*.iconfont.cn/*
 // @require      http://cdn.bootcss.com/jquery/1.12.4/jquery.min.js
-// @updateURL    https://github.com/yee94/tampermonkey-iconfont-iconify/raw/main/index.user.js
-// @downloadURL  https://github.com/yee94/tampermonkey-iconfont-iconify/raw/main/index.user.js
-// @require  	 https://github.com/yee94/tampermonkey-iconfont-iconify/raw/main/dist/main.js
+// @updateURL    https://github.com/Daybreak-1024/tampermonkey-iconfont-iconify/raw/main/index.user.js
+// @downloadURL  https://github.com/Daybreak-1024/tampermonkey-iconfont-iconify/raw/main/index.user.js
+// @require  	 https://github.com/Daybreak-1024/tampermonkey-iconfont-iconify/raw/main/dist/main.js
 // @grant        unsafeWindow
 // @connect      *
 // ==/UserScript==
